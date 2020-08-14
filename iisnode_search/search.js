9d5c116c8e9040e291a528cb116e8e4aa2d2f607
thinhmascot <67301649+thinhmascot@users.noreply.github.com>
2020-08-14 15:02:33 +0700

    s

---
+++ iisnode_search/search.js
@@ -0,0 +1,18 @@
+﻿//const PORT = process.env.PORT;
+const PORT = 8081;
+
+const _ = require('lodash');
+const HTTP = require('http');
+const CACHES = [];
+
+const SERVER = HTTP.createServer(function (req, res) {
+    let time = _.VERSION;
+
+    res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+    res.write(time, "utf-8");
+    res.end();
+});
+
+SERVER.listen(PORT, function () {
+    console.log(PORT);
+});
